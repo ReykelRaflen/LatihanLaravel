d55c0358844a47875160c3730e2867f2d26641bf
Reykel Raflen Awang <reykel210618@gmail.com>
2025-03-11 00:34:58 +0700

    ubah dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -14,7 +14,7 @@
 
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Dashboard1122</h1>
+                    <h1 class="m-0">Dashboard1123</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
